c68013b9f3d0bda5bb80e84f21545f7ed1536d74
browsable <wlsehfdl1>
2017-03-22 20:51:35 +0900

    init
    first commit

---
+++ web/app.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var path = require('path');
+var router = require('./router/main');
+var bodyParser = require('body-parser');
+var redis = require('redis');
+//fs = require("fs");
+
+var client = redis.createClient('6379', 'redis');
+
+// client.on("error", function (err) {
+//     console.log("Error " + err);
+// });
+//
+// client.set("string key", "string val", redis.print);
+// client.hset("hash key", "hashtest 1", "some value", redis.print);
+// client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+// client.hkeys("hash key", function (err, replies) {
+//     console.log(replies.length + " replies:");
+//     replies.forEach(function (reply, i) {
+//         console.log("    " + i + ": " + reply);
+//     });
+//     client.quit();
+// });
+var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('port',process.env.PORT || 8080);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', router);
+app.use('/test', router);
+
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
+var server = app.listen(app.get('port'), function(){
+    console.log('Express server listening on port ' +server.address().port)
+});
+
+
---
+++ web/lib/db_pool.js
@@ -0,0 +1,16 @@
+/**
+ * Created by daemin Hwnag on 2017-01-10.
+ */
+
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
+    connectionLimit : 100, //important
+    host : "db",
+    port : 3306,
+    user : "root",
+    password : "dexign1234",
+    database : "dexign"
+});
+  // Dexign
+module.exports = pool;
