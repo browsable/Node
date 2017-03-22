95effd61bf10e8c115d29fce814a5d76738ffdee
browsable <wlsehfdl1>
2017-03-22 21:14:03 +0900

    docker-compose install shell script added

---
+++ automation/docker-compose_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)"
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose -v
